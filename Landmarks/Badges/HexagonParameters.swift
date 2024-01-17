0ba3996ff573cee0c324dae6791d05dc7ccb11ec
MauiDesign <36174695+MauiDesign@users.noreply.github.com>
2024-01-18 11:44:19 +1300

    Update HexagonParameters.swift

--- Landmarks/Badges/HexagonParameters.swift
+++ Landmarks/Badges/HexagonParameters.swift
@@ -18,32 +18,32 @@ struct HexagonParameters {
     // Hexagon draw anti clock wise
     static let segments = [
         Segment(
-            line:    CGPoint(x: 0.60, y: 0.05),//Bottom-middle (start curve vertex)
-            curve:   CGPoint(x: 0.40, y: 0.05),//Bottom-middle (end curve vertex)
+            line:    CGPoint(x: 0.60, y: 0.05),//Top-middle (start curve vertex)
+            curve:   CGPoint(x: 0.40, y: 0.05),//Top-middle (end curve vertex)
             control: CGPoint(x: 0.50, y: 0.00)
         ),
         Segment(
-            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),// Bottom-left (start curve vertex)
-            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),// Bottom-left (end curve vertex)
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),// Top-left (start curve vertex)
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),// Top-left (end curve vertex)
             control: CGPoint(x: 0.00, y: 0.25 + adjustment)
         ),
         Segment(
-            line:    CGPoint(x: 0.00, y: 0.70 + adjustment),// Top-right (start curve vertex)
-            curve:   CGPoint(x: 0.05, y: 0.80 + adjustment),// Top-right (end curve vertex)
-            control: CGPoint(x: 0.00, y: 0.75 + adjustment)
+            line:    CGPoint(x: 0.00, y: 0.60 + adjustment),// Bottom-left (start curve vertex)
+            curve:   CGPoint(x: 0.05, y: 0.70 + adjustment),// Bottom-left (end curve vertex)
+            control: CGPoint(x: 0.00, y: 0.627 + adjustment)
         ),
         Segment(
-            line:    CGPoint(x: 0.40, y: 0.95),// top-middle (start curve vertex)
-            curve:   CGPoint(x: 0.60, y: 0.95),// top-middle (end curve vertex)
+            line:    CGPoint(x: 0.40, y: 0.95),// Bottom-middle (start curve vertex)
+            curve:   CGPoint(x: 0.60, y: 0.95),// Bottom-middle (end curve vertex)
             control: CGPoint(x: 0.50, y: 1.00)
         ),
         Segment(
-            line:    CGPoint(x: 0.95, y: 0.80 + adjustment),// top-left (start curve vertex)
-            curve:   CGPoint(x: 1.00, y: 0.70 + adjustment),// top-left (end curve vertex)
-            control: CGPoint(x: 1.00, y: 0.75 + adjustment)
+            line:    CGPoint(x: 0.95, y: 0.70 + adjustment),// Bottom-right (start curve vertex)
+            curve:   CGPoint(x: 1.00, y: 0.60 + adjustment),// Bottom-right (end curve vertex)
+            control: CGPoint(x: 1.00, y: 0.627 + adjustment)
         ),
         Segment(
-            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),// Botton-left (start curve vertex)
+            line:    CGPoint(x: 1.00, y: 0.40 + adjustment),// Top-left (start curve vertex)
             curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),// Top-left (start curve vertex)
             control: CGPoint(x: 1.00, y: 0.25 + adjustment)
         )
