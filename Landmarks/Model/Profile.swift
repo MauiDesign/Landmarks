0f9b231e6fba1380b41d06b78c5befe9ae851860
MauiDesign <36174695+MauiDesign@users.noreply.github.com>
2024-02-06 21:29:11 +1300

    Profile update

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,13 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Aroha Design on 6/02/24.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotificat = true
+}
